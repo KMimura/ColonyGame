8a0ed213d44bb9c9c63d1ebf5132470a385a5b38
KMimura <mimurakohei256@gmail.com>
2021-08-13 14:11:32 +0900

    create a simple map

---
+++ game.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"image/color"
+	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/EngoEngine/engo/common"
+	"github.com/KMimura/ColonyGame/systems"
+)
+
+type MainScene struct{}
+
+func run() {
+	opts := engo.RunOptions{
+		Title:          "ColonyGame",
+		Width:          900,
+		Height:         600,
+		StandardInputs: true,
+		NotResizable:   true,
+	}
+	engo.Run(opts, &MainScene{})
+}
+
+func (*MainScene) Type() string { return "mainScene" }
+
+func (*MainScene) Preload() {
+	engo.Files.Load("pics/overworld_tileset_grass.png")
+	// engo.Files.LoadReaderData("go.ttf", bytes.NewReader(gosmallcaps.TTF))
+	common.SetBackground(color.RGBA{255, 250, 220, 0})
+}
+
+func (*MainScene) Setup(u engo.Updater) {
+	engo.Input.RegisterButton("MoveRight", engo.KeyD, engo.KeyArrowRight)
+	engo.Input.RegisterButton("MoveLeft", engo.KeyA, engo.KeyArrowLeft)
+	engo.Input.RegisterButton("MoveUp", engo.KeyW, engo.KeyArrowUp)
+	engo.Input.RegisterButton("MoveDown", engo.KeyS, engo.KeyArrowDown)
+	engo.Input.RegisterButton("Space", engo.KeySpace)
+	world, _ := u.(*ecs.World)
+	world.AddSystem(&common.RenderSystem{})
+	world.AddSystem(&systems.SceneSystem{})
+	// world.AddSystem(&systems.PlayerSystem{})
+	// world.AddSystem(&systems.EnemySystem{})
+	// world.AddSystem(&systems.BulletSystem{})
+	// world.AddSystem(&systems.IntermissionSystem{})
+}
+
+func (*MainScene) Exit() {
+	engo.Exit()
+}
+
+func main() {
+	run()
+}
---
+++ systems/SceneSystem.go
@@ -0,0 +1,93 @@
+package systems
+
+import (
+	// "math/rand"
+	"fmt"
+	"reflect"
+	// "time"
+	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/EngoEngine/engo/common"
+)
+
+// Spritesheet タイルの画像
+var Spritesheet *common.Spritesheet
+
+// camEntity カメラシステムのエンティティ
+var camEntity *common.CameraSystem
+
+// cameraInitialPositionX,Y カメラの初期位置
+var cameraInitialPositionX int
+var cameraInitialPositionY int
+
+// cellLength セル一辺のピクセル数（必ず16の倍数にすること）
+var cellLength = 80
+
+// Tile タイル一つ一つを表す構造体
+type Tile struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+// SceneSystem シーンシステム
+type SceneSystem struct {
+	world   *ecs.World
+	texture *common.Texture
+}
+
+// Remove 削除する
+func (ss *SceneSystem) Remove(entity ecs.BasicEntity) {
+	for _, system := range ss.world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Remove(entity)
+		}
+	}
+}
+
+// Update アップデートする
+func (ss *SceneSystem) Update(dt float32) {
+}
+
+// New 作成時に呼び出される
+func (ss *SceneSystem) New(w *ecs.World) {
+	ss.Init(w)
+}
+
+// Init 初期化
+func (ss *SceneSystem) Init(w *ecs.World) {
+	// rand.Seed(time.Now().UnixNano())
+	ss.world = w
+	// 素材シートの読み込み
+	loadTxt := "pics/overworld_tileset_grass.png"
+	Spritesheet = common.NewSpritesheetWithBorderFromFile(loadTxt, 16, 16, 0, 0)
+	fmt.Println(Spritesheet.CellCount())
+	for _, system := range ss.world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			for i := 0; i < cellLength; i++ {
+				for j := 0; j < cellLength; j++ {
+					tile := &Tile{BasicEntity: ecs.NewBasic()}
+					tile.SpaceComponent.Position = engo.Point{
+						X: float32(j * cellLength),
+						Y: float32(i * cellLength),
+					}
+					tile.RenderComponent = common.RenderComponent{
+						Drawable: Spritesheet.Cell(0),
+						Scale:    engo.Point{X: float32(cellLength / 16), Y: float32(cellLength / 16)},
+					}
+					tile.RenderComponent.SetZIndex(0)
+					sys.Add(&tile.BasicEntity, &tile.RenderComponent, &tile.SpaceComponent)
+				}
+			}
+		}
+	}
+	// カメラエンティティの取得
+	for _, system := range w.Systems() {
+		fmt.Println(reflect.TypeOf(system))
+		switch sys := system.(type) {
+		case *common.CameraSystem:
+			camEntity = sys
+		}
+	}
+}
