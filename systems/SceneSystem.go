9f4c19afd6d3ec78756708c3466a16505beabc41
KMimura <mimurakohei256@gmail.com>
2021-08-15 23:23:08 +0900

    create map based on a struct data

--- systems/SceneSystem.go
+++ systems/SceneSystem.go
@@ -1,7 +1,6 @@
 package systems
 
 import (
-	"fmt"
 	"math/rand"
 
 	// "reflect"
@@ -23,7 +22,9 @@ var cameraInitialPositionX int
 var cameraInitialPositionY int
 
 // cellLength セル一辺のピクセル数（必ず16の倍数にすること）
-var cellLength = 80
+const cellLength = 48
+
+const screenLength = 50
 
 // Tile タイル一つ一つを表す構造体
 type Tile struct {
@@ -69,7 +70,6 @@ func (ss *SceneSystem) Update(dt float32) {
 			Incremental: true,
 		})
 	} else if engo.Input.Button("MoveRight").Down() {
-		fmt.Println(camEntity.X())
 		engo.Mailbox.Dispatch(common.CameraMessage{
 			Axis:        common.XAxis,
 			Value:       16,
@@ -90,18 +90,28 @@ func (ss *SceneSystem) init(w *ecs.World) {
 	// 素材シートの読み込み
 	loadTxt := "pics/overworld_tileset_grass.png"
 	Spritesheet = common.NewSpritesheetWithBorderFromFile(loadTxt, 16, 16, 0, 0)
+	// createRiver(ss.world)
 	for _, system := range ss.world.Systems() {
 		switch sys := system.(type) {
 		case *common.RenderSystem:
-			for i := 0; i < 100; i++ {
-				for j := 0; j < 100; j++ {
+			type tileInfo struct {
+				spritesheetNum int
+			}
+			var stage_tiles [screenLength][screenLength]tileInfo
+			for i, s := range stage_tiles {
+				for j, _ := range s {
+					stage_tiles[i][j].spritesheetNum = rand.Intn(4)
+				}
+			}
+			for i, s := range stage_tiles {
+				for j, y := range s {
 					tile := &Tile{BasicEntity: ecs.NewBasic()}
 					tile.SpaceComponent.Position = engo.Point{
-						X: float32(j*cellLength - cellLength),
-						Y: float32(i*cellLength - cellLength),
+						X: float32(j * cellLength),
+						Y: float32(i * cellLength),
 					}
 					tile.RenderComponent = common.RenderComponent{
-						Drawable: Spritesheet.Cell(rand.Intn(4)),
+						Drawable: Spritesheet.Cell(y.spritesheetNum),
 						Scale:    engo.Point{X: float32(cellLength / 16), Y: float32(cellLength / 16)},
 					}
 					tile.RenderComponent.SetZIndex(0)
@@ -110,24 +120,25 @@ func (ss *SceneSystem) init(w *ecs.World) {
 			}
 		}
 	}
-	createRiver(ss.world)
 	// カメラエンティティの取得
 	for _, system := range w.Systems() {
 		switch sys := system.(type) {
 		case *common.CameraSystem:
 			camEntity = sys
-			common.CameraBounds.Max.X = float32(1000 * 16)
-			common.CameraBounds.Max.Y = float32(1000 * 16)
+			common.CameraBounds.Max.X = float32(screenLength * cellLength)
+			common.CameraBounds.Max.Y = float32(screenLength * cellLength)
 		}
 	}
 }
 
-func createRiver(w *ecs.World) {
-	rand.Seed(time.Now().UnixNano())
-	// 川が垂直方向で始まるか水平方向で始まるか
-	if_vertical := false
-	if rand.Intn(2) == 1 {
-		if_vertical = true
-	}
-	fmt.Println(if_vertical)
-}
+// func createRiver(w *ecs.World) []int {
+// 	rand.Seed(time.Now().UnixNano())
+// 	// 川が垂直方向で始まるか水平方向で始まるか
+// 	if_vertical := false
+// 	if rand.Intn(2) == 1 {
+// 		if_vertical = true
+// 	}
+// 	var start_points [2]int
+// 	// 川の始まりの地点の選択
+// 	river_start_point := rand.Intn(screenLength/2) + screenLength/4
+// }
